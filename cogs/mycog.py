ae0edf6b05a274ac447203c39d072073689912b5
mgni <soemintun007@gmail.com>
2018-01-09 17:50:28 +0900

    Update mycog.py

--- cogs/mycog.py
+++ cogs/mycog.py
@@ -12,6 +12,25 @@ class Mycog:
         """This does stuff!"""
 
         #Your code will go here
+
+        data.add_field(name="This is name", value="This is value")
+        data.add_field(name="This is name 2", value="This is value 2")
+#       data.set_footer(text="Member #{} | User ID:{}"
+#"".format(member_number, user.id)) "
+
+        if user.avatar_url:
+            data.set_author(name=name, url=user.avatar_url)
+            data.set_thumbnail(url=user.avatar_url)
+        else:
+            data.set_author(name=name)
+
+        try:
+            await self.bot.say(embed=data)
+        except discord.HTTPException:
+            await self.bot.say("I need the `Embed links` permission "
+"to send this")
+            
+            
         await self.bot.say("I can do stuff!")
 
 def setup(bot):
