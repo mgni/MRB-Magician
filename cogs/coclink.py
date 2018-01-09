8276c47409d019c3bccb7c208e73f961f2f64708
mgni <soemintun007@gmail.com>
2018-01-10 03:12:32 +0900

    Update coclink.py

--- cogs/coclink.py
+++ cogs/coclink.py
@@ -1,77 +1,32 @@
-
+import discord
 from discord.ext import commands
-from .utils.dataIO import dataIO
-from .utils import checks
-from .utils.chat_formatting import pagify, box
-import os
-import re
 
 class Coclink:
-    """coc player info"""
-
-        def __init__(self, bot):
+    def __init__(self, bot):
         self.bot = bot
 
-@commands.command(pass_context=True, no_pm=True)
-    async def testinfo(self, ctx, *, user: discord.Member=None):
-        """Shows users's informations"""
+    @commands.command(pass_context=True, no_pm=True)
+    async def whois(self, ctx, *, user: discord.Member=None):
+        """Shows mycom informations"""
+        
         author = ctx.message.author
         server = ctx.message.server
-
+  
         if not user:
             user = author
-
-        roles = [x.name for x in user.roles if x.name != "@everyone"]
-        
-        server = ctx.message.server
-  
-        joined_at = self.fetch_joined_at(user, server)
-        since_created = (ctx.message.timestamp - user.created_at).days
-        since_joined = (ctx.message.timestamp - joined_at).days
-        user_joined = joined_at.strftime("%d %b %Y %H:%M")
-        user_created = user.created_at.strftime("%d %b %Y %H:%M")
-        member_number = sorted(server.members,
-                               key=lambda m: m.joined_at).index(user) + 1
-
-        created_on = "{}\n({} days ago)".format(user_created, since_created)
-        joined_on = "{}\n({} days ago)".format(user_joined, since_joined)
-
-        data = discord.Embed(description=game, colour=user.colour)
-        data.add_field(name="Joined Discord on", value=created_on)
-        data.add_field(name="Joined this server on", value=joined_on)
-        data.add_field(name="Roles", value=roles, inline=False)
-#        data.set_footer(text="Member #{} | User ID:{}"
+        data = discord.Embed(colour=user.colour)
+        data.add_field(name="Name", value="This is value")
+        data.add_field(name="Name 2", value="This is value 2")
+        data.add_field(name="Sample link", value="This is sample link [Red, an open source Discord bot](https://discord.gg/red)", inline=False)
+#       data.set_footer(text="Member #{} | User ID:{}"
 #"".format(member_number, user.id)) "
-        
-       game = "Chilling in {} status".format(user.status)
-
-        if user.game is None:
-            pass
-        elif user.game.url is None:
-            game = "Playing {}".format(user.game)
-        else:
-            game = "Streaming: [{}]({})".format(user.game, user.game.url)
-
-        if roles:
-            roles = sorted(roles, key=[x.name for x in server.role_hierarchy
-                                       if x.name != "@everyone"].index)
-            roles = ", ".join(roles)
-        else:
-            roles = "None"
-
-        data = discord.Embed(description=game, colour=user.colour)
-        data.add_field(name="Joined Discord on", value=created_on)
-        data.add_field(name="Joined this server on", value=joined_on)
-        data.add_field(name="Roles", value=roles, inline=False)
-        """data.set_footer(text="Member #{} | User ID:{}"
-                             "".format(member_number, user.id))"""
 
         name = str(user)
-        name = " ~ ".join((name, user.nick)) if user.nick else name
-
-        if user.avatar_url:
-            data.set_author(name=name, url=user.avatar_url)
-            data.set_thumbnail(url=user.avatar_url)
+        if user.avatar:
+            avatar = user.avatar_url
+            avatar = avatar.replace('webp','png')
+            data.set_author(name=name, url=avatar)
+            data.set_thumbnail(url=avatar)
         else:
             data.set_author(name=name)
 
@@ -80,10 +35,6 @@ class Coclink:
         except discord.HTTPException:
             await self.bot.say("I need the `Embed links` permission "
 "to send this")
-        
-        print(author.name + " has been set as owner.")
-
+            
 def setup(bot):
-#    check_folders()
-#    check_files()
-bot.add_cog(coclink(bot))
+    bot.add_cog(Coclink(bot))
