adb8f02cf6aa642fa3260f4ae4d00d283f590844
mgni <soemintun007@gmail.com>
2018-01-10 23:06:45 +0900

    Update mycog.py

--- cogs/mycog.py
+++ cogs/mycog.py
@@ -38,7 +38,6 @@ class Mybot:
             await self.bot.say("I need the `Embed links` permission "
 "to send this")
             
-    @commands.command(pass_context=True, no_pm=True)
     async def clanmsg(self, ctx, *, user: discord.Member=None):
         """Shows clan message for MM Redy Blood clan"""
         
@@ -48,14 +47,13 @@ class Mybot:
         if not user:
             user = author
             
-            mmrb_link = "https://link.clashofclans.com/?action=OpenClanProfile&tag=#82U2R290"
-            msgdata_mmrb=("Thi is MMRB Body Text.\nTH9 : 15/15\nTH0 : 35/35\nTH1 : 40/40/20\n")
-            member_number=10
-            msgdata_footer="Members 30/50 | Win/Loss/Ties: 160/72/0"
+           member_number=10
+           msgdata_footer="Members 30/50 | Win/Loss/Ties: 160/72/0"
             
         data = new discord.Embed(colour=user.colour)
-        data.add_field(name="[MM Redy Blood](https://link.clashofclans.com/?action=OpenClanProfile&tag=#82U2R290)", value="", inline=False)
-        data.add_field(name="[Goto Clan](https://link.clashofclans.com/?action=OpenClanProfile&tag=#82U2R290)", value=msgdata_mmrb, inline=False)
+        data.add_field(name="MM Redy Blood", value="")
+        data.add_field(name="[Goto Clan](https://link.clashofclans.com/?action=OpenClanProfile&tag=#82U2R290)", value="", inline=False)
+        data.add_field(name="", value="Body Text Here", inline=False)
         data.add_field(name="<:bk2:400623664162406411>Leader:", value="LeaderName")
         data.add_field(name="TH levels accepted:", value="<:th9:400623665005330442> <:th10:400623664195829770> <:th11:400623664824844288>")
         data.add_field(name="Allowed in war:", value="<:th9:400623665005330442> = <:bk:400623663809822730> <:aq:400623663952560128>\n"
