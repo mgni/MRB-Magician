eb9e8b4d3b4d07588324a2365a82eb6de3133d1c
mgni <soemintun007@gmail.com>
2018-01-10 19:24:42 +0900

    Update mycog.py

--- cogs/mycog.py
+++ cogs/mycog.py
@@ -1,34 +1,25 @@
 import discord
 from discord.ext import commands
-#from .utils.chat_formatting import escape_mass_mentions, italics, pagify
-#from random import randint
-#from random import choice
-#from enum import Enum
-#from urllib.parse import quote_plus
-#import datetime
-#import time
-#import aiohttp
-#import asyncio
 
 class Mybot:
     def __init__(self, bot):
-        """Init."""
         self.bot = bot
-        
+
     @commands.command(pass_context=True, no_pm=True)
-    async def mycom(self, ctx, *, user: discord.Member=None):
+    async def whois(self, ctx, *, user: discord.Member=None):
         """Shows mycom informations"""
+        
         author = ctx.message.author
         server = ctx.message.server
-                
+  
         if not user:
             user = author
-        
         data = discord.Embed(colour=user.colour)
-        data.add_field(name="This is name", value="This is value")
-        data.add_field(name="This is name 2", value="This is value 2")
-        data.add_field(name="sample", value="This is sample2, [Red, an open source Discord bot](https://discord.gg/red)", inline=False)
-        #data.add_field(name="Trophies", value="This is Trophy Value <:barbarian:316393732671012864>", inline=False)
+        data.add_field(name="Name", value="This is value")
+        data.add_field(name="Name 2", value="This is value 2")
+        data.add_field(name="Sample link", value="This is sample link [Red, an open source Discord bot](https://discord.gg/red)", inline=False)
+#       data.set_footer(text="Member #{} | User ID:{}"
+#"".format(member_number, user.id)) "
 
         name = str(user)
         if user.avatar:
@@ -45,8 +36,5 @@ class Mybot:
             await self.bot.say("I need the `Embed links` permission "
 "to send this")
             
-            
- #       await self.bot.say("I can do stuff!")
-
 def setup(bot):
     bot.add_cog(Mycog(bot))
